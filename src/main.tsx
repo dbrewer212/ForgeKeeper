88e4c04d03ccdf94ee2ebb77e22f8c9e64cf7cea
dbrewer212 <dbrewer212@gmail.com>
2026-08-11 19:28:09 -0400

    Initialize Foundry mesh with application startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { getFoundryMeshRuntime } from "./mesh";
+
+const mesh = getFoundryMeshRuntime();
+void mesh
+  .initialize()
+  .then(() => mesh.save())
+  .catch((error) => {
+    console.error("Foundry mesh initialization failed.", error);
+  });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
