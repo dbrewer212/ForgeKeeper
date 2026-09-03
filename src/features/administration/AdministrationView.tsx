d1c40f2bf3e5e58496e1cc64f9872c06d83199b2
dbrewer212 <dbrewer212@gmail.com>
2026-09-03 10:14:57 -0400

    Keep Foundry Link hosting desktop-only

--- src/features/administration/AdministrationView.tsx
+++ src/features/administration/AdministrationView.tsx
@@ -1,12 +1,14 @@
 import { FoundryLinkPanel } from "./FoundryLinkPanel";
 import { SettingsView } from "../settings/SettingsView";
 import { RecoveryAuditView } from "../recovery/RecoveryAuditView";
+import { isFoundryMobileRuntime } from "../../platform/runtime";
 import type { ForgekeeperState } from "../../state/useForgekeeperState";
 
 export function AdministrationView({ state }: { state: ForgekeeperState }) {
+  const mobile = isFoundryMobileRuntime();
   return (
     <div className="space-y-8">
-      <FoundryLinkPanel state={state} />
+      {!mobile ? <FoundryLinkPanel state={state} /> : null}
       <SettingsView state={state} />
       <RecoveryAuditView state={state} />
     </div>
